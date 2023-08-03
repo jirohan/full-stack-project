d11e57d96f5d11fb148a22cecaf53aa7b680da7f
Rohan Maharjan <hondazmr6@gmail.com>
2023-08-03 18:01:10 +0545

    router-dom-config

--- jobs/client/src/App.jsx
+++ jobs/client/src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Navbar, Footer } from './components'
+import {Outlet, Navigate, Route, Routes, useLocation} from 'react-router-dom'
+
+const Layout = () => {
+  const user = true;
+  const location = useLocation()
+
+  return user ? (
+   <Outlet/>
+   ) : (
+    <Navigate to='user-auth' state={{from: location }} replace/>
+  );
+}
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Navbar />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route 
+            path='/' 
+            element={<Navigate to='/find-jobs' replace={true} />} 
+            />
+            <Route path='/find-jobs' element={<FindJobs />} />
+            <Route path='/compaines' element={<Companies />} />
+          </Route>
+        </Routes>
+      <Footer />
+    </main>
   )
 }
 
