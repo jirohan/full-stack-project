da6b4194fcf40b1c7a450057bc2174dc297b7028
Rohan Maharjan <hondazmr6@gmail.com>
2023-08-16 16:22:36 +0545

    login

--- ecommerce/frontend/src/pages/home/auth/LoginPage.jsx
+++ ecommerce/frontend/src/pages/home/auth/LoginPage.jsx
@@ -1,12 +1,41 @@
 import React, { useState } from 'react'
 import { HeaderComponent } from '../../../components/HeaderComponent'
 import { Container, Col, Row, Form, Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
     let [data, setData] = useState({
-        email: "teat@user.com",
-        password: "admin123"
+        email: null,
+        password: null
     })
+
+    let navigate = useNavigate()
+
+    const handleChange = (e) => {
+            let {name, type, value, checked, files} = e.target;
+            setData({
+                ...data,
+                [e.target.name]: e.target.value
+            })
+        }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Data:", data)
+        //API integration
+        let user_detail = {result: {
+           user:  {_id: 12, name: "rajina", email: "rajinamaharjan@gmail.com", password: "admin123", role:"seller"},
+           token: "jwttoken"
+        }};
+        //local storage
+        //5mb
+        //string, no time limit
+        localStorage.setItem("_ecomm_user", JSON.stringify(user_detail))
+        //cookie => a domain can have 50 cookie, every cookie size is generally 4096 characters
+        //string can only be stored, certain time
+        //if success = ?? dashboard / admin
+        navigate("/"+user_detail.result.user.role)
+    }    
   return (
     <>
         <HeaderComponent />
@@ -19,18 +48,30 @@ const LoginPage = () => {
             </Row>
             <Row>
                 <Col>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                                <Form.Control size='sm' defaultValue={data.email} type="email" placeholder="Enter email" />
+                                <Form.Control onChange={handleChange} size='sm' defaultValue={data.email} type="email" placeholder="Enter email" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control defaultValue={data.password} type="password" placeholder="Password" />
+                            <Form.Control onChange={handleChange} defaultValue={data.password} type="password" placeholder="Password" />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
+                        <Col sm={{offset:3, span: 9}}>
+                            <Button variant="danger" type="reset" className='me-1' size='sm'>
+                            <i className='fa fa-trash'></i>&nbsp;
+                                Submit
+                            </Button>
+                            <Button variant="success" type="submit" size='sm'>
+                            <i className='fa fa-paper-plane'></i>&nbsp;
+                                Login
+                            </Button>
+                            {/* <NavLink className={"btn btn-sm btn-success"} to="/admin">
+                            <i className='fa fa-paper-plane'></i>&nbsp;
+                                Login
+                            </NavLink> */}
+                        </Col>
+                        
                     </Form>
                 </Col>
             </Row>
