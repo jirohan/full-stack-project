d7787f33c9996f07c04f460ed9b59a1310b0eceb
Rohan Maharjan <hondazmr6@gmail.com>
2023-08-22 16:50:32 +0545

    login

--- ecommerce/api/app.js
+++ ecommerce/api/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 
+// Auth and Authorization
+// Eg: Login, Register, Logout, forget Password, Dashboard, Activation User
+
+app.post("/login", (req, res)=>{
+    // TODO: Valdiation, Modeling, DB Query,  
+    let validation_flag = false;
+    if(!validation_flag){
+        res.status(400).json({
+            result: null,
+            status: false,
+            msg: "Credential doesnt match"
+        });
+    } else{
+        res.json({
+            result: null,
+            status: true,
+            msg: "Login Success"
+        });
+    }
+});
+
 app.listen(3005, 'localhost', (err) => {
     if(!err) {
         console.log("Server is listening to port 3005");
