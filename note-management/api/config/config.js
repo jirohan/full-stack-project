009e60858ed2f9da665a693652ba4ca8ae46f4ba
Rohan Maharjan <hondazmr6@gmail.com>
2023-09-04 22:14:54 +0545

    config

---
+++ note-management/api/config/config.js
@@ -0,0 +1,13 @@
+const Config= {
+    ENVIRONMENT:"dev",
+    DB: {
+        PROTOCOL: "mongodb",
+        HOST: "127.0.0.1",
+        NAME: "note-management",
+        USER: "",
+        PWD: "",
+        PORT: 27017
+    }
+}
+
+module.exports = Config;
