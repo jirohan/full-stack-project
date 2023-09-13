57b32be1282a2aeba80707e339e029c6c78cdf8c
Rohan Maharjan <hondazmr6@gmail.com>
2023-09-13 22:48:13 +0545

    nepal data

---
+++ note-management/api/config/nepal-data.config.js
@@ -0,0 +1,8 @@
+const NepalData={
+    state: ["State 1",'Madesh Pradesh','Bagmati Pradesh','Gandaki Pradesh','Lumbini Pradesh','Karnali Pradesh',"Sudur Paschim Pradesh"],
+    district: ["Kathmandu",'Lalitpur','Bhaktapur'],
+    municipality: ["Kathmandu Ma. Na. pa.","Kageshwori manohara Na. pa. ", "Lalitpur Ma. Na. Pa. "]
+
+}
+
+module.exports = NepalData;
