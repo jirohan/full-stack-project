ca0d905daf6466f46010d42e77d371c26ae51b17
Rohan Maharjan <hondazmr6@gmail.com>
2023-10-03 16:19:14 +0545

    featured

---
+++ gutpau bed and breakfast/client/src/components/featured/Featured.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./featured.css"
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+        <div className="featuredItem">
+            <img src="" alt="" className='featuredImg'/>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
