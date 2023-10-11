c9ad43a8accab77202f8f4e7332000b3f2339c0f
Rohan Maharjan <hondazmr6@gmail.com>
2023-10-11 09:04:36 +0545

    initial commit

---
+++ E-shop/froend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {LoginPage } from "./Routes"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
