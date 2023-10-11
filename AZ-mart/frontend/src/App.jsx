3897609e62f3818b79ee86edba6cf8b9c45730bd
Rohan Maharjan <hondazmr6@gmail.com>
2023-10-11 09:01:47 +0545

    initial commit

---
+++ AZ-mart/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/header/Navbar'
+import Newnav from './components/newnavbar/Newnav'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Newnav />
+    </>
+  )
+}
+
+export default App
---
+++ AZ-mart/frontend/src/components/home/Maincomp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Maincomp = () => {
+  return (
+    <div>Maincomp</div>
+  )
+}
+
+export default Maincomp
